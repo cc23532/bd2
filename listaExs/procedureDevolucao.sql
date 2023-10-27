eba6fa00cbedfd42dd51c7b185d01f59c1e4da32
matheuscarletti03 <142463172+matheuscarletti03@users.noreply.github.com>
2023-10-27 19:22:09 -0300

    Update procedureDevolucao.sql

--- listaExs/procedureDevolucao.sql
+++ listaExs/procedureDevolucao.sql
@@ -5,20 +5,32 @@ as
 begin
     declare @diaAtual date,
             @dataDevPrevista date,
+            @multaDiaria money,
             @multa money,
+            @multiplicadorMulta int,
             @suspenso char(1),
             @dataDevReal date
-    
+
     set @resultado= 'NAO'
     set @diaAtual= getdate()
-    select bib.multa m
+    select @multaDiaria= multaDiaria from bib.multa
 
 begin try
     if exists ( select * from bib.Emprestimo where idLivro= @idLivro and dataDevolucaoReal is null)
     begin
-        if datediff(day, dataDevolucaoPrevista, @diaAtual)>15
-        begin
-        set @multa= m.multadiaria * datediff(day, dataDevolucaoPrevista, @diaAtual)
+        if datediff(day, dataDevolucaoPrevista, @diaAtual)>0
+        begin   
+            set @multiplicadorMulta= datediff(day, dataDevolucaoPrevista, @diaAtual)
+            set @multa= @multiplicadorMulta * @multaDiaria
+            update bib.leitor
+            set multa= @multa
+            where idLeitor= @idLeitor
+            if datediff(day, dataDevolucaoPrevista, @diaAtual)>15
+            begin
+                update bib.leitor
+                set suspenso= 's'
+                where idLeitor= @idLeitor
+            end
         end
     end
 end try
